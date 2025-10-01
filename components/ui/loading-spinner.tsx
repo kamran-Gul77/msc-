38224d1643e2e1b53e3cdd6b3de94c5b06a03c93
kamran-Gul77 <wpwizard777@gmail.com>
2025-10-01 20:25:16 +0500

    d

--- components/ui/loading-spinner.tsx
+++
@@ -1,8 +0,0 @@
-export function LoadingSpinner() {
-  return (
-    <div className="flex items-center space-x-2">
-      <div className="w-8 h-8 border-4 border-blue-200 border-t-blue-600 rounded-full animate-spin"></div>
-      <span className="text-gray-600">Loading...</span>
-    </div>
-  );
-}
